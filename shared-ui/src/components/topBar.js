08376d1f99c46123de48e796ca50c1542fd31577
MTY621 <mty210205@gmail.com>
2026-09-01 11:29:27 +0300

    ﻿Archive quotation requests and refine demo and quotation UI

--- shared-ui/src/components/topBar.js
+++ shared-ui/src/components/topBar.js
@@ -84,7 +84,7 @@ export function renderTopBar({ brandSrc, brandAlt, projectName, state, capabilit
         <button class="book-demo-button" type="button" data-action="book-demo" aria-label="Book a demo">
           <span class="book-demo-button__label">Book a demo</span>
           <span class="book-demo-button__sparkles" aria-hidden="true">
-            <i></i><i></i><i></i><i></i>
+            <i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i>
           </span>
         </button>
       </div>
