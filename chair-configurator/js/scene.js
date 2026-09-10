398da3f9f7418accd44ecc32c10d005d94355c6c
MTY621 <mty210205@gmail.com>
2026-09-10 15:18:31 +0300

    ﻿chair: remove the remaining front arm support gap

--- chair-configurator/js/scene.js
+++ chair-configurator/js/scene.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { createSurfaceSystem } from '../../shared-3d/src/index.js?v=platform-19';
-import { createChairModel } from './chairGeometry.js?v=chair-23';
-import { registerChairMaterials } from './materials.js?v=chair-23';
+import { createChairModel } from './chairGeometry.js?v=chair-24';
+import { registerChairMaterials } from './materials.js?v=chair-24';
 
 export class ChairScene {
   constructor(container, { state, onCameraChange = () => {} } = {}) {
