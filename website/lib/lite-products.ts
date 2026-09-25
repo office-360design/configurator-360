b6a15204c8cae6f1d5f09369b1f76a6c22c0f89f
laptop <vlad.raul.vasile@gmail.com>
2026-09-25 17:06:38 +0300

    change tiles preview

--- website/lib/lite-products.ts
+++ website/lib/lite-products.ts
@@ -10,5 +10,5 @@ export const liteDefaults: Record<LiteSlug, LiteState> = {
   chair: { wood: "#be8851", woodType:"oak", fabric: "#b88162", weave: "linen" },
   cardbox: { style: "standard", width: 600, depth: 400, height: 300, open: 0, colour: "#b88959", paper:"TFT",top:"simple-flaps",bottom:"simple-flaps" },
   bookshelf: { family: "compact", layout: "straight", count: 2, doors: "lower", colour: "#b98555", hardware:"diamond", doorOpen:"closed", shelves:9 },
-  tiles: { length: 3, width: 2, tile: "parket", pattern: "running", colour: "#969a98", houseEnabled:"no", houseShape:"rectangle", houseLength:1.5, houseWidth:1, houseRotation:0,curbs:"no" },
+  tiles: { length: 3, width: 2, tile: "parket", pattern: "running", rotation:0, colour: "#969a98", curbs:"no" },
 };
